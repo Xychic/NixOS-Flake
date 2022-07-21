a2a1ca8cc15120d3b99d78a821ef01f8fd314cab
Jacob Turner <jacob11turner@gmail.com>
2022-07-21 22:34:13 +0000

    Add discord

---
+++ home/gui/discord/default.nix
@@ -0,0 +1,22 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  home.packages = let
+  in [
+    (
+      pkgs.callPackage ./discord.nix {}
+    )
+  ];
+
+  home.file.".config/discord/settings.json" = {
+    text = builtins.toJSON {
+      "BACKGROUND_COLOR" = "#202225";
+      "IS_MAXIMIZED" = true;
+      "IS_MINIMIZED" = false;
+      # Discord likes to break old versions. Don't do that
+      "SKIP_HOST_UPDATE" = true;
+    };
+  };
+}
