f6028d90399dc9b510ad1327c33576157e2dea86
Jacob Turner <jacob11turner@gmail.com>
2022-07-22 09:40:38 +0000

    Format files

--- flake.nix
+++ flake.nix
@@ -21,44 +21,43 @@
     home-manager,
     ...
   }: let 
-   nixpkgsConfig = {
-     config = {
-       allowUnfree = true;
-     };
-   };
-  in
-    {
-      # Lenovo-V330
-      nixosConfigurations.V330 = nixpkgs.lib.nixosSystem rec {
-        system = "x86_64-linux";
-        specialArgs = {
-          systemName = "V330";
-          inherit inputs;
-        };
-        modules = [
-          ./V330
-          home-manager.nixosModules.home-manager
-          (
-            {
-              pkgs,
-              home-manager,
-              ...
-            }: {
-              home-manager = {
-                extraSpecialArgs = specialArgs;
-                users.jacob = {
-                  home.stateVersion = "21.11";
-                  nixpkgs = nixpkgsConfig;
-                  imports = [
-                    ./home/cli/core
-                    ./home/gui/vscode
-                    ./home/gui/discord
-                  ];
-                };
-              };
-            }
-          )
-        ];
+    nixpkgsConfig = {
+      config = {
+        allowUnfree = true;
       };
     };
+  in {
+    # Lenovo-V330
+    nixosConfigurations.V330 = nixpkgs.lib.nixosSystem rec {
+      system = "x86_64-linux";
+      specialArgs = {
+        systemName = "V330";
+        inherit inputs;
+      };
+      modules = [
+        ./V330
+        home-manager.nixosModules.home-manager
+        (
+          {
+            pkgs,
+            home-manager,
+            ...
+          }: {
+            home-manager = {
+              extraSpecialArgs = specialArgs;
+              users.jacob = {
+                home.stateVersion = "21.11";
+                nixpkgs = nixpkgsConfig;
+                imports = [
+                  ./home/cli/core
+                  ./home/gui/vscode
+                  ./home/gui/discord
+                ];
+              };
+            };
+          }
+        )
+      ];
+    };
+  };
 }
---
+++ home/cli/core/git.nix
@@ -0,0 +1,12 @@
+{pkgs, ...} : {
+  home.packages = with pkgs;[ git ];
+  programs.git = {
+    enable = true;
+    userName = "Jacob Turner";
+    userEmail = "jacob11turner@gmail.com";
+    difftastic.enable = true;
+    extraConfig = {
+      init.defaultBranch = "master";
+    };
+  };
+}
