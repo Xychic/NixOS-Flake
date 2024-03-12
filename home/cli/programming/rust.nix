a4a356fad590b07862a2dc430347680db91c787a
Jacob Turner <jacob11turner@gmail.com>
2024-03-12 16:14:17 +0000

    Update flake Note due to [CVE-2024-27297](https://cve.mitre.org/cgi-bin/cvename.cgi\?name\=2024-27297), you have to `export NIXPKGS_ALLOW_INSECURE=1` and add the `--impure` tag

--- home/cli/programming/rust.nix
+++ home/cli/programming/rust.nix
@@ -2,8 +2,8 @@
   nixpkgs.overlays = [
     (import "${
         fetchTarball {
-            url = "https://github.com/nix-community/fenix/archive/4b07da0f91ea99f263f47165a11a48678c9e0dc3.tar.gz";
-            sha256 = "sha256:13rmlpf7igvilg1xx1jrrcvjvviabbgjlby4g9dvvxx8fr3xwz9i";
+            url = "https://github.com/nix-community/fenix/archive/12222a90a7e02443f822e679055858d786e7324f.tar.gz";
+            sha256 = "sha256:1l818fc5x296hlma8zb5g1n2xwx0k6fxnk7x8dzbgk6px0d9bx9h";
         }
     }/overlay.nix")
   ];
